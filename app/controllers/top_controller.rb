b8c799e38628d9eb4eb2058dfa0e92be7a5ffda0
tech-camp <tech-camp@tech-camp-1-141-no-MacBook-Air.local>
2016-09-20 21:31:22 +0900

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,8 @@
+class TopController < ApplicationController
+
+  def index
+    @question = Question.new
+    feed_contents = current_user.group.feed_contents.includes(:content)
+    @feed_contents = feed_contents.map(&:content)
+  end
+end
